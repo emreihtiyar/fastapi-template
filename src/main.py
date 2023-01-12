1538d18b7cb75789ac1fbee6128ddc98940b4d14
emreihtiyar <ihti1@gmail.com>
2023-01-12 23:06:57 +0300

    base Oauth2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from configs import ApiConfigs
 
 app = FastAPI(
@@ -10,6 +11,9 @@ app = FastAPI(
     openapi_url=ApiConfigs.api_openapi_url,
 )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=ApiConfigs.api_base_url+"/token")
+
+
 @app.get(ApiConfigs.api_base_url+"/")
 def get_root():
     return "API is up and running"
@@ -17,3 +21,11 @@ def get_root():
 @app.post(ApiConfigs.api_base_url+"/")
 def post_root():
     return "API is up and running"
+
+@app.post(ApiConfigs.api_base_url+"/token")
+async def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return {"access_token": form_data.username+"token"}
+
+@app.get(ApiConfigs.api_base_url+"/test")
+async def test(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
