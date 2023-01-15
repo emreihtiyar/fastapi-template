7102ff88845c3e24a3a04e5ef8f34bb6f585db95
emreihtiyar <emre.ihti1@gmail.com>
2023-01-15 17:22:59 +0300

    initialize Hashing Context

--- src/security/hashing/__init__.py
+++ src/security/hashing/__init__.py
@@ -2,4 +2,12 @@ from passlib.hash import bcrypt, bcrypt_sha256, argon2, scrypt
 from passlib.context import CryptContext
 from configs import HashingConfigs
 
-pwd_context = CryptContext(
+class Hashing:
+    """Hashing class to hash and verify passwords"""
+    def __init__(self) -> None:
+        self.context = CryptContext(
+            schemes=[HashingConfigs.algorithm],
+            default=HashingConfigs.algorithm,
+            all__vary_rounds=HashingConfigs.rounds,
+            all__salt=HashingConfigs.salt,
+        )
